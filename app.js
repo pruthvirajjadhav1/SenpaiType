23b1ed4120d9d563a7b34f47733fddfe1e9c05f7
khushigarg1 <khushigarg.64901@gmail.com>
2022-10-30 22:22:28 +0530

    made changes

--- app.js
+++ app.js
@@ -1900,7 +1900,7 @@ document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
 const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
 const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
 
-var theme = "dark";
+var theme = "light";
   const root = document.querySelector(":root");
   const container = document.getElementsByClassName("theme-container")[0];
   const themeIcon = document.getElementById("theme-icon");
